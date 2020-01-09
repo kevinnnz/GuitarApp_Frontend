7f02f7bed27b759f919e509f6be78e3797af8a03
Kevin Z <zaworskk@sheridancollege.ca>
2020-01-08 19:03:32 -0500

    react env variables

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 
 // firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBl8vMtZqPbI1PUPVdNUlPGhtV3pVOgMEg",
-    authDomain: "guitarapp-a7d23.firebaseapp.com",
-    databaseURL: "https://guitarapp-a7d23.firebaseio.com",
-    projectId: "guitarapp-a7d23",
-    storageBucket: "guitarapp-a7d23.appspot.com",
-    messagingSenderId: "178706043908",
-    appId: "1:178706043908:web:6a3368ec0a06758b0f5151",
-    measurementId: "G-YF4LJDTY3V"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASEURL,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
